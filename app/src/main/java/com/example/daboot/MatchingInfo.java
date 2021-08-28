8630b0a017f0dcb478968dacf426cd825088ccb1
miinn <gg04253@gmail.com>
2021-08-28 19:58:54 +0900

    게시판, 매칭 탭 기능 및 디자인 개선

--- app/src/main/java/com/example/daboot/MatchingInfo.java
+++ app/src/main/java/com/example/daboot/MatchingInfo.java
@@ -1,6 +1,8 @@
 package com.example.daboot;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,23 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MatchingInfo extends AppCompatActivity {
 
+    private Button btn_back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matching_info);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();//상단바 제거
+
+        btn_back = findViewById(R.id.btn_matching_info_back);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/daboot/MatchingList.java
+++ app/src/main/java/com/example/daboot/MatchingList.java
@@ -2,6 +2,9 @@ package com.example.daboot;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
@@ -9,12 +12,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MatchingList extends AppCompatActivity {
 
+    private Button btn_back;
+    private LinearLayout tmp; //todo: 나중에 이름 바꿀것
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matching_list);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();//상단바 제거
+
+        btn_back = findViewById(R.id.btn_matching_list_back);
+
+        tmp = findViewById(R.id.temp_id); //todo: 나중에 이름 바꿀것
+
+        //뒤로가기
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        tmp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MatchingInfo.class);
+                startActivity(intent);
+            }
+        });
 
         /*
         TextView tv_name = (TextView)findViewById(R.id.tv_name);
@@ -37,6 +63,7 @@ public class MatchingList extends AppCompatActivity {
         String field = intent.getExtras().getString("field");
         tv_field.setText(field);
         */
+
 
 
     }
