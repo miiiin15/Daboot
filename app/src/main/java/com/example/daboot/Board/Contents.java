199c9c9bf8daf366579d8eb6da4e24d57b5394c7
miinn <gg04253@gmail.com>
2021-08-29 21:19:03 +0900

    가짜값 추가

--- app/src/main/java/com/example/daboot/Board/Contents.java
+++ app/src/main/java/com/example/daboot/Board/Contents.java
@@ -3,6 +3,8 @@ package com.example.daboot.Board;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
@@ -13,6 +15,8 @@ import com.example.daboot.R;
 public class Contents extends AppCompatActivity {
 
     private Button btn_back;
+    private EditText edt_coments;
+    private LinearLayout coments_bar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,6 +26,10 @@ public class Contents extends AppCompatActivity {
         actionBar.hide();//상단바 제거
 
         btn_back = findViewById(R.id.btn_board_contents_back);
+
+        edt_coments = findViewById(R.id.edt_board_contents_coment);
+
+        coments_bar = findViewById(R.id.board_contents_coments_BAR);
 
 
         btn_back.setOnClickListener(new View.OnClickListener() {
